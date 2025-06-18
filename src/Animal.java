1013c45a8da1c82042d24e27bad805cde2524639
Rikard <rikardengstrom89@gmail.com>
2025-06-18 09:35:44 +0200

    Working with inheritence, polymorphism.

--- src/Animal.java
+++ src/Animal.java
@@ -1,11 +1,12 @@
 public class Animal {
 
-    private String type;
+    protected String type;
     private String size;
     private double weight;
 
 
-    public Animal() {}
+    public Animal() {
+    }
 
     public Animal(String type, String size, double weight) {
         this.type = type;
@@ -25,6 +26,7 @@ public class Animal {
     public void move(String speed) {
         System.out.println(type + " moves " + speed);
     }
+
     public void makeNoise() {
         System.out.println(type + " makes some kind of noise");
     }
--- src/Dog.java
+++ src/Dog.java
@@ -27,11 +27,40 @@ public class Dog extends Animal {
 
     public void makeNoise() {
 
+        if (type == "Wolf") {
+            System.out.println("Ow Wooo!");
+        }
+        bark();
+        System.out.println();
     }
 
     @Override
     public void move(String speed) {
         super.move(speed);
-        System.out.println("Dogs walk, run and wag their tail.");
+        if (speed == "slow") {
+            walk();
+            wagTail();
+        } else {
+            run();
+            bark();
+        }
+        System.out.println();
+//        System.out.println("Dogs walk, run and wag their tail.");
+    }
+
+    private void bark() {
+        System.out.print("Woof! ");
+    }
+
+    private void run() {
+        System.out.print("Dog running.. ");
+    }
+
+    private void walk() {
+        System.out.print("Dog walking.. ");
+    }
+
+    private void wagTail() {
+        System.out.print("Dog wagging tail.. ");
     }
 }
